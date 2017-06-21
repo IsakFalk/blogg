6892b4fce6f0c85f896f744cf4e31967ba94b386
Isak Falk <isak.falk@live.se>
2017-06-21 12:01:41 +0100

    Fixed up some of the directories

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,7 +23,10 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('This site', 'http://isakfalk.com'))
+
+# Theme
+THEME = 'theme'
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -33,3 +36,6 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Cache
+LOAD_CONTENT_CACHE = False
